6a2523f0eadf749d60e262349e5de5dae69c431a
Dmitry Gladkov <dmitrygla@mellanox.com>
2019-09-19 09:58:51 +0300

    fix

--- src/uct/tcp/tcp_iface.c
+++ src/uct/tcp/tcp_iface.c
@@ -238,6 +238,7 @@ void uct_tcp_iface_outstanding_dec(uct_tcp_iface_t *iface)
 void uct_tcp_iface_conn_quota_release(uct_tcp_iface_t *iface)
 {
     uct_tcp_ep_pending_list_elem_t *pending_ep;
+    ucs_status_t status;
 
     iface->conn_quota++;
     ucs_assert(iface->conn_quota <= iface->config.max_conn_quota);
@@ -246,6 +247,12 @@ void uct_tcp_iface_conn_quota_release(uct_tcp_iface_t *iface)
         pending_ep = ucs_list_extract_head(&iface->pending_ep_list,
                                            uct_tcp_ep_pending_list_elem_t,
                                            list);
+
+        status = uct_tcp_iface_conn_quota_acquire(iface, pending_ep->ep);
+        if (status != UCS_OK) {
+            return;
+        }
+
         uct_tcp_cm_conn_start(pending_ep->ep);
         ucs_free(pending_ep);
     }
