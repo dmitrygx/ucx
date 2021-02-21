e9bb6987458b2e6fbefd886dfe3ff01adba6d380
dmitrygx <dmitrygla@mellanox.com>
2021-02-21 14:39:01 +0200

    UCT/CUDA: Select the first device (if exists) if user doesn't set

--- src/uct/cuda/base/cuda_iface.c
+++ src/uct/cuda/base/cuda_iface.c
@@ -17,8 +17,14 @@ uct_cuda_base_query_devices(uct_md_h md, uct_tl_device_resource_t **tl_devices_p
 {
     ucs_sys_device_t sys_device = UCS_SYS_DEVICE_ID_UNKNOWN;
     CUdevice cuda_device;
+    int device_count;
 
-    if (cuCtxGetDevice(&cuda_device) == CUDA_SUCCESS) {
+    if ((cuCtxGetDevice(&cuda_device) == CUDA_SUCCESS) ||
+        /* if there is no selected device by user, get system info for the first
+         * as a workaround */
+        ((cudaGetDeviceCount(&device_count) == cudaSuccess) &&
+         (device_count > 0) &&
+         (cuDeviceGet(&cuda_device, 0) == CUDA_SUCCESS))) {
         uct_cuda_base_get_sys_dev(cuda_device, &sys_device);
     }
 
