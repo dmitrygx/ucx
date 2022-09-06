de84cb40bca5a0376eb331bbe84528d692e489bc
dmitrygx <dmitrygla@mellanox.com>
2022-09-06 23:10:50 +0300

    UCT/API: Introduce exported mkey buffer

--- src/uct/rocm/ipc/rocm_ipc_md.c
+++ src/uct/rocm/ipc/rocm_ipc_md.c
@@ -47,9 +47,9 @@ uct_rocm_ipc_md_query(uct_md_h md, uct_md_attr_t *md_attr,
 static ucs_status_t
 uct_rocm_ipc_mkey_pack(uct_md_h uct_md, uct_mem_h memh,
                        const uct_md_mkey_pack_params_t *params,
-                       void *rkey_buffer)
+                       void *buffer)
 {
-    uct_rocm_ipc_key_t *packed = rkey_buffer;
+    uct_rocm_ipc_key_t *packed = buffer;
     uct_rocm_ipc_key_t *key    = memh;
 
     *packed = *key;
