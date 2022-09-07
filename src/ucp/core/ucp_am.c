03743ad058a49c04899f977e8a10ef2945456201
dmitrygx <dmitrygla@mellanox.com>
2022-09-07 18:26:23 +0300

    UCP/CORE: Implement exported/imported memh

--- src/ucp/core/ucp_am.c
+++ src/ucp/core/ucp_am.c
@@ -987,6 +987,19 @@ UCS_PROFILE_FUNC(ucs_status_ptr_t, ucp_am_send_nbx,
         op_id     = UCP_OP_ID_AM_SEND;
     }
 
+    /* Make Coverity happy */
+    ucs_assert((param->op_attr_mask & UCP_OP_ATTR_FIELD_MEMH) || 
+               (param->memh == NULL));
+    if (ucs_unlikely((param->op_attr_mask & UCP_OP_ATTR_FIELD_MEMH) &&
+                     (param->memh->flags & UCP_MEM_FLAG_IMPORTED))) {
+        if (ucs_unlikely(flags & UCP_AM_SEND_FLAG_EAGER)) {
+            ret = UCS_STATUS_PTR(UCS_ERR_INVALID_PARAM);
+            goto out;
+        }
+
+        flags |= UCP_AM_SEND_FLAG_RNDV;
+    }
+
     if (ucs_likely(attr_mask == 0)) {
         status = ucp_am_try_send_short(ep, id, flags, header, header_length,
                                        buffer, count, max_short);
--- src/ucp/wireup/select.c
+++ src/ucp/wireup/select.c
@@ -997,6 +997,15 @@ static void ucp_wireup_fill_peer_err_criteria(ucp_wireup_criteria_t *criteria,
     }
 }
 
+static void
+ucp_wireup_fill_exported_memh_criteria(ucp_context_h context,
+                                       ucp_wireup_criteria_t *criteria)
+{
+    if (context->config.features & UCP_FEATURE_EXPORTED_MEMH) {
+        criteria->local_md_flags |= UCT_MD_FLAG_EXPORTED_MKEY;
+    }
+}
+
 static double ucp_wireup_aux_score_func(const ucp_worker_iface_t *wiface,
                                         const uct_md_attr_v2_t *md_attr,
                                         const ucp_address_entry_t *remote_addr,
@@ -1059,9 +1068,11 @@ static void ucp_wireup_criteria_init(ucp_wireup_criteria_t *criteria)
 /**
  * Check whether emulation over AM is allowed for RMA/AMO lanes
  */
-static int ucp_wireup_allow_am_emulation_layer(unsigned ep_init_flags)
+static int ucp_wireup_allow_am_emulation_layer(ucp_context_h context,
+                                               unsigned ep_init_flags)
 {
-    return !(ep_init_flags & UCP_EP_INIT_FLAG_MEM_TYPE);
+    return !(ep_init_flags & UCP_EP_INIT_FLAG_MEM_TYPE) &&
+           !(context->config.features & UCP_FEATURE_EXPORTED_MEMH);
 }
 
 static unsigned
@@ -1094,6 +1105,8 @@ static ucs_status_t
 ucp_wireup_add_rma_lanes(const ucp_wireup_select_params_t *select_params,
                          ucp_wireup_select_context_t *select_ctx)
 {
+    ucp_ep_h ep                    = select_params->ep;
+    ucp_context_h context          = ep->worker->context;
     unsigned ep_init_flags         = ucp_wireup_ep_init_flags(select_params,
                                                               select_ctx);
     ucp_wireup_criteria_t criteria = {};
@@ -1127,6 +1140,7 @@ ucp_wireup_add_rma_lanes(const ucp_wireup_select_params_t *select_params,
     }
     criteria.calc_score             = ucp_wireup_rma_score_func;
     ucp_wireup_fill_peer_err_criteria(&criteria, ep_init_flags);
+    ucp_wireup_fill_exported_memh_criteria(context, &criteria);
 
     tl_bitmap = ucp_tl_bitmap_max;
     for (mem_type = 0; mem_type < UCS_MEMORY_TYPE_LAST; ++mem_type) {
@@ -1179,6 +1193,7 @@ ucp_wireup_add_amo_lanes(const ucp_wireup_select_params_t *select_params,
     ucp_wireup_init_select_flags(&criteria.local_iface_flags,
                                  UCT_IFACE_FLAG_PENDING, 0);
     ucp_wireup_fill_peer_err_criteria(&criteria, ep_init_flags);
+    ucp_wireup_fill_exported_memh_criteria(context, &criteria);
     ucp_context_uct_atomic_iface_flags(context, &criteria.remote_atomic_flags);
 
     /* We can use only non-p2p resources or resources which are explicitly
@@ -1642,6 +1657,7 @@ ucp_wireup_add_rma_bw_lanes(const ucp_wireup_select_params_t *select_params,
     ucp_wireup_init_select_flags(&bw_info.criteria.local_iface_flags,
                                  UCT_IFACE_FLAG_PENDING, 0);
     ucp_wireup_fill_peer_err_criteria(&bw_info.criteria, ep_init_flags);
+    ucp_wireup_fill_exported_memh_criteria(context, &bw_info.criteria);
 
     if (ucs_test_all_flags(ucp_ep_get_context_features(ep),
                            UCP_FEATURE_TAG | UCP_FEATURE_WAKEUP)) {
@@ -1666,6 +1682,8 @@ ucp_wireup_add_rma_bw_lanes(const ucp_wireup_select_params_t *select_params,
         bw_info.criteria.local_md_flags |= UCT_MD_FLAG_RKEY_PTR;
         bw_info.criteria.lane_type       = UCP_LANE_TYPE_RKEY_PTR;
         bw_info.max_lanes                = 1;
+        ucp_wireup_fill_peer_err_criteria(&bw_info.criteria, ep_init_flags);
+        ucp_wireup_fill_exported_memh_criteria(context, &bw_info.criteria);
 
         UCP_CONTEXT_MEM_CAP_TLS(context, UCS_MEMORY_TYPE_HOST, access_mem_types,
                                 tl_bitmap);
@@ -1677,6 +1695,8 @@ ucp_wireup_add_rma_bw_lanes(const ucp_wireup_select_params_t *select_params,
     bw_info.criteria.lane_type      = UCP_LANE_TYPE_RMA_BW;
     bw_info.max_lanes               = context->config.ext.max_rndv_lanes;
     bw_info.criteria.local_md_flags = md_reg_flag;
+    ucp_wireup_fill_peer_err_criteria(&bw_info.criteria, ep_init_flags);
+    ucp_wireup_fill_exported_memh_criteria(context, &bw_info.criteria);
 
     /* If error handling is requested we require memory invalidation
      * support to provide correct data integrity in case of error */
@@ -1703,6 +1723,8 @@ ucp_wireup_add_rma_bw_lanes(const ucp_wireup_select_params_t *select_params,
         /* Set the new iface RMA flags */
         ucp_wireup_criteria_iface_flags_add(&bw_info.criteria, &iface_rma_flags,
                                             &peer_rma_flags);
+        ucp_wireup_fill_peer_err_criteria(&bw_info.criteria, ep_init_flags);
+        ucp_wireup_fill_exported_memh_criteria(context, &bw_info.criteria);
 
         /* Add lanes that can access the memory by short operations */
         added_lanes = 0;
@@ -1858,7 +1880,8 @@ ucp_wireup_select_params_init(ucp_wireup_select_params_t *select_params,
     select_params->tl_bitmap     = tl_bitmap;
     select_params->address       = remote_address;
     select_params->allow_am      =
-            ucp_wireup_allow_am_emulation_layer(ep_init_flags);
+            ucp_wireup_allow_am_emulation_layer(ep->worker->context,
+                                                ep_init_flags);
     select_params->show_error    = show_error;
 }
 
